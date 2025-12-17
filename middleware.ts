26ce21de637fc76426f20dd8a593173d1531d881
sakamoto <sakamotojp.nicedesign@gmail.com>
2025-12-17 09:53:42 +0900

    add middleware

---
+++ middleware.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export const config = {
+  matcher: [
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:png|jpg|jpeg|webp|gif|svg|ico)$).*)",
+  ],
+};
+
+export function middleware(request: NextRequest) {
+  const basicAuth = request.headers.get("authorization");
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(" ")[1];
+    const [user, pass] = atob(authValue).split(":");
+
+    if (
+      user === process.env.BASIC_AUTH_USER &&
+      pass === process.env.BASIC_AUTH_PASSWORD
+    ) {
+      return NextResponse.next();
+    }
+  }
+
+  return new NextResponse("Authentication required", {
+    status: 401,
+    headers: {
+      "WWW-Authenticate": 'Basic realm="Secure Area"',
+    },
+  });
+}
